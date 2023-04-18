0ac68c915502cb050d4f6b16807eceeebc2f4da9
Josh Petras <josh@petr.as>
2023-04-18 10:24:13 -0500

    Change column def for decimal ID to fix searchbuilder bug.

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,10 +91,11 @@
           }
         },
         {
-          "data": "tpid",
+          "data": null,
           "render": function(data, type, row) {
+            var hexId = row.tpid;
             // Extract the hexadecimal part of the tpid
-            var hex = data.split("-")[1];
+            var hex = hexId.split("-")[1];
             // Convert the hexadecimal to decimal and adjust for TM numbering methodology subtracting d000 or 53248.
             var decimal = parseInt(hex, 16) - 53248;
             return decimal;
